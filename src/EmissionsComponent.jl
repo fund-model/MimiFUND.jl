af3c6deb108e21523b32e3fb8b0e8e9c3c4a49d0
David Anthoff <anthoff@berkeley.edu>
2014-04-23 15:01:32 -0700

    Progress

--- src/EmissionsComponent.jl
+++ src/EmissionsComponent.jl
@@ -1,16 +1,4 @@
-﻿// FUND - Climate Framework for Uncertainty, Negotiation and Distribution
-// Copyright (C) 2012 David Anthoff and Richard S.J. Tol
-// http://www.fund-model.org
-// Licensed under the MIT license
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Esmf;
-using Fund.CommonDimensions;
-
-namespace Fund.Components
-{
+﻿using IAMF
 
     public interface IEmissionsState
     {
