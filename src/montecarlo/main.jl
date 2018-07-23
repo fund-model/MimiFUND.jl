7d912ff37d31d52695831590fc84767909c87444
Cora Kingdon <ckingdon@berkeley.edu>
2018-07-23 09:16:16 -0400

    Basic FUND mcs test

--- src/montecarlo/main.jl
+++ src/montecarlo/main.jl
@@ -1,23 +1,28 @@
 using Mimi
 
-samplesize = 10
-
 include(joinpath(dirname(@__FILE__), "defmcs.jl"))
 include(joinpath(dirname(@__FILE__), "../fund.jl"))
-
-# Import default fund model
 using fund
-default_fund = fund.FUND 
-run(default_fund)
+
+trials = 10
 
 # Set up output directories
 output = joinpath(dirname(@__FILE__), "../../output/", Dates.format(now(), "yyyy-mm-dd HH-MM-SS"))
-mkdir(output)
-mkdir("$output/trials")
-mkdir("$output/results")
+mkpath("$output/trials")
+mkpath("$output/results")
 
 # Generate trials
-generate_trials!(mcs, samplesize; filename = joinpath(@__DIR__, "$output/trials/fund_mc_trials_$samplesize.csv"))
+generate_trials!(mcs, trials; filename = joinpath(@__DIR__, "$output/trials/fund_mc_trials_$trials.csv"))
+
+# TEMPORARY post trial function to test apply trial data
+function post(mcs::MonteCarloSimulation, trialnum::Int, ntimesteps::Int, tup::Void)
+    m = mcs.models[1]
+    println(m[:climatedynamics, :climatesensitivity]) # should be printing out the trial values but it's the same default value every time
+    return nothing
+end
 
 # Run monte carlo trials
-run_mcs(mcs, default_fund; output_dir = "$output/results")
+set_model!(mcs, FUND)
+run_mcs(mcs; 
+    post_trial_func = post, 
+    output_dir = "$output/results")
--- src/montecarlo/scc_mcs.jl
+++ src/montecarlo/scc_mcs.jl
@@ -1,39 +1,89 @@
 using Mimi
 
-samplesize = 10
+include(joinpath(dirname(@__FILE__), "defmcs.jl"))                  # Defines FUND's distributional parameters
+# include(joinpath(dirname(@__FILE__), "../new_marginaldamages.jl"))  # Defines FUND's marginal emissions model
 
-include(joinpath(dirname(@__FILE__), "defmcs.jl"))
-include(joinpath(dirname(@__FILE__), "scc_mcs_tools.jl"))
-include(joinpath(dirname(@__FILE__), "../new_marginaldamages.jl"))
+function run_FUND_SCC(trials = 10; year = 2010, years = nothing, rate = 0.03, rates = nothing)
 
+    # Set up output directories
+    output = joinpath(dirname(@__FILE__), "../../output/", "SCC $(Dates.format(now(), "yyyy-mm-dd HH-MM-SS"))")
+    mkpath("$output/trials")
+    mkpath("$output/results")
 
+    # Write header in SCC output file
+    scc_file = joinpath(output, "scc.csv")
+    open(scc_file, "w") do f 
+        write(f, "trial, rate, year, scc\n")
+    end 
 
-# Scenario set up
-scenario_args = [
-    :rate           => [0.03]
-    # :emissionyear   => [2010]
-]
-# _rate           = 0.03
-_emissionyear   = 2010
+    # Scenario set up
+    scenario_args = [
+        :rate           => rates != nothing ? rates : [rate]
+        :emissionyear   => years != nothing ? years : [year]
+    ]
 
-# Set up output directories
-output = joinpath(dirname(@__FILE__), "../../output/", "SCC $(Dates.format(now(), "yyyy-mm-dd HH-MM-SS"))")
-mkpath("$output/trials")
-mkpath("$output/results")
+    # Generate trials
+    generate_trials!(mcs, trials; filename = joinpath(@__DIR__, "$output/trials/fund_mc_trials_$trials.csv"))
+    
+    # Get FUND marginal model
+    mm = create_marginal_FUND_model()
+    set_model!(mcs, mm)
 
-# Get FUND marginal model
-mm = create_marginal_FUND_model()
+    # Define scenario function
+    function _scenario_func(mcs::MonteCarloSimulation, tup::Tuple)
+        
+        # Unpack scenario tuple argument
+        (rate, emissionyear) = tup
+    
+        # Get models
+        base, marginal = mcs.models
+    
+        # Perturb emissons in the marginal model
+        ci = marginal.mi.components[:marginalemission]
+        emissions = Mimi.get_parameter_value(ci, :add)
+        emissions[:] = 0
+        emissions[getindexfromyear(emissionyear):getindexfromyear(emissionyear) + 9] = 1.0
+    
+        nothing
+    end
 
-# Generate trials
-generate_trials!(mcs, samplesize; filename = joinpath(@__DIR__, "$output/trials/fund_mc_trials_$samplesize.csv"))
+    # Define post trial function
+    function scc_calculation(mcs::MonteCarloSimulation, trialnum::Int, ntimesteps::Int, tup::Tuple)
 
-# Run monte carlo trials
-set_model!(mcs, mm)
-run_mcs(mcs; 
-    output_dir = "$output/results",
-    scenario_args = scenario_args, 
-    scenario_func = _scenario_func,
-    pre_trial_func = pre_trial_func,
-    post_trial_func = scc_calculation
-    )
+        # Unpack scenario tuple argument
+        (rate, emissionyear) = tup
+    
+        # Get marginal damages
+        base, marginal = mcs.models
+        marginaldamages = (marginal[:impactaggregation, :loss] - base[:impactaggregation, :loss]) / 10000000.0
+    
+        println(base[:climatedynamics, :climatesensitivity]) #DELETE
+
+        # Calculate discount factor
+        T = ntimesteps == typemax(Int) ? length(dimension(base, :time)) : ntimesteps
+        discount_factor = zeros(T)
+        idx = getindexfromyear(emissionyear)
+        discount_factor[idx:T] = [1 / ((1 + rate) ^ t) for t in 0:T-idx]
+    
+        # Sum discounted damages to scc
+        scc = sum(sum(marginaldamages, 2)[2:end] .* discount_factor[2:end])
+    
+        # Write output
+        open(scc_file, "a") do f 
+            write(f, "$trialnum, $rate, $emissionyear, $scc\n")
+        end 
+    
+        return nothing
+    end
+
+    # Run monte carlo trials
+    run_mcs(mcs, trials; 
+        output_dir = "$output/results",
+        scenario_args = scenario_args, 
+        scenario_func = _scenario_func,   
+        post_trial_func = scc_calculation   
+        )
+
+    return nothing
+end 
 
