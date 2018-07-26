8a15f9250397450fd57d80a0f82eeaadc961a5ff
Cora Kingdon <ckingdon@berkeley.edu>
2018-07-26 16:59:39 -0400

    Implements load_mcs_RVs() for formatting FUND's mcs parameters

--- src/montecarlo/load_mcs_RVs.jl
+++ src/montecarlo/load_mcs_RVs.jl
@@ -1,12 +1,32 @@
 using DataStructures
 
-txt_out = "src/montecarlo/mcs_RVs.txt"
-datadir = "data"
+"""
+This function loads the FUND input parameters stored in datadir into the syntax
+needed for the @defmcs macro. This script writes to a text file which can then be copied into a @defmcs macro.
+"""
+function load_mcs_RVs(txt_out = joinpath(@__DIR__, "mcs_RVs.txt"), datadir = joinpath(@__DIR__, "../../data"))
 
-files = readdir(datadir)
-filter!(i->i!="desktop.ini", files)
-parameters = OrderedDict{Any, Any}(splitext(file)[1] => readdlm(joinpath(datadir,file), ',') for file in files)
+    files = readdir(datadir)
+    filter!(i->i!="desktop.ini", files)
+    parameters = OrderedDict{Any, Any}(splitext(file)[1] => readdlm(joinpath(datadir,file), ',') for file in files)
 
+    open(txt_out, "w") do f 
+        for (k, v) in parameters
+            val = prepparameter(v)
+            if val != nothing
+                write(f, "$k = $val\n")
+            end
+        end 
+    end
+
+end
+
+"""
+Converts one distribution as defined in FUND's datafiles into a string of a julia-parsable distribution definition.
+Example: "~N(0.089;0.1484;min=0)" becomes "Truncated(Normal(0.089,0.1484), 0.0, Inf)"
+If pv is a scalar or boolean, it returns nothing.
+If pv starts with '~', but is not ~N(), ~Gamma(), or ~Triangular(), it throws an error. 
+"""
 function convert(pv)
     if isa(pv,AbstractString)
         if startswith(pv,"~") & endswith(pv,")")
@@ -48,66 +68,42 @@ function convert(pv)
             else
                 error("Unknown distribution")
             end
-        elseif pv=="true"
-            return true
-        elseif pv=="false"
-            return false
-        elseif endswith(pv, "y")
-            return parse(Int, strip(pv,'y'))
-        else
-            try
-                return parse(Float64, pv)
-            catch e
-                error(pv)
-            end
         end
-        return "$pv"
-    else
-        return nothing #"$pv"
     end
+    return nothing
 end
 
-cat(x,y)=string(x,", ",y)
+# Helper function for concatenating strings in prepparameter
+_cat(x,y)=string(x,", ",y)
+
+"""
+Takes the contents of one file and turns it into the string needed for a definition within a @defmcs macro.
+Input 'p' may be a scalar, a 1-D array (defined as two columns in long format), or a 2-D array (defined as three columns in long format).
+If p contains no distributional values, then this returns nothing.
+"""
 function prepparameter(p)
     column_count = size(p,2)
     if column_count == 1
-        val = convert(p[1,1])
+        return convert(p[1,1])
     elseif column_count == 2
-        if isa(p[1,2], SubString)
-            val = "[$(reduce(cat, [convert(p[j,2]) for j in 1:size(p,1)]))]"
-        else 
-            return nothing 
-        end
-    elseif column_count == 3
-        if isa(p[1,3], SubString)
-            length_index1 = length(unique(p[:,1]))
-            length_index2 = length(unique(p[:,2]))
-            new_p = Array{String}(length_index1,length_index2)
-            cur_1 = 1
-            cur_2 = 1
-            for j in 1:size(p,1)
-                new_p[cur_1,cur_2] = convert(p[j,3])
-                cur_2 += 1
-                if cur_2 > length_index2
-                    cur_2 = 1
-                    cur_1 += 1
-                end
+        vals = []
+        for i in 1:size(p, 1)
+            dist = convert(p[i, 2])
+            if dist !== nothing 
+                push!(vals, "$(p[i,1]) => $dist")
             end
-            val = new_p
-        else
-            return nothing
         end
+        return isempty(vals) ? nothing : "[$(reduce(_cat, vals))]"
+    elseif column_count == 3
+        vals = []
+        for i in 1:size(p, 1)
+            dist = convert(p[i, 3])
+            if dist !== nothing
+                push!(vals, "[$(p[i,1]), $(p[i,2])] => $dist")
+            end
+        end
+        return isempty(vals) ? nothing : "[$(reduce(_cat, vals))]"
+    else
+        error("Unable to parse data files with $column_count columns.")
     end
-    return val
 end
-
-f = open(txt_out, "w")
-
-for (k,v) in parameters
-    val = prepparameter(v)
-    if val != nothing
-        write(f, "$k = $val\n")
-    end
-end 
-
-close(f)
