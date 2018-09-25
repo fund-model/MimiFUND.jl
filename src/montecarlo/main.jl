0f2882bfce7818c4b4779ae96a08183d52f35efb
Cora Kingdon <ckingdon@berkeley.edu>
2018-09-25 11:32:36 -0400

    Make run_fund_mcs function in main

--- src/montecarlo/main.jl
+++ src/montecarlo/main.jl
@@ -1,28 +1,24 @@
 using Mimi
 
-include(joinpath(dirname(@__FILE__), "defmcs.jl"))
-include(joinpath(dirname(@__FILE__), "../fund.jl"))
+include(joinpath(@__DIR__, "defmcs.jl"))
+include(joinpath(@__DIR__, "../fund.jl"))
 using fund
 
-trials = 10
 
-# Set up output directories
-output = joinpath(dirname(@__FILE__), "../../output/", Dates.format(now(), "yyyy-mm-dd HH-MM-SS"))
-mkpath("$output/trials")
-mkpath("$output/results")
+function run_fund_mcs(trials = 10; output = nothing, save_trials = false)
 
-# Generate trials
-generate_trials!(mcs, trials; filename = joinpath(@__DIR__, "$output/trials/fund_mc_trials_$trials.csv"))
+    # Set up output directories
+    output = joinpath(@__DIR__, "../../output/", Dates.format(now(), "yyyy-mm-dd HH-MM-SS"))
+    mkpath("$output/results")
 
-# TEMPORARY post trial function to test apply trial data
-function post(mcs::MonteCarloSimulation, trialnum::Int, ntimesteps::Int, tup::Void)
-    m = mcs.models[1]
-    println(m[:climatedynamics, :climatesensitivity]) # should be printing out the trial values but it's the same default value every time
-    return nothing
+    # Generate trials
+    if save_trials
+        mkpath("$output/trials")
+        generate_trials!(mcs, trials; filename = joinpath(output, "trials/fund_mc_trials_$trials.csv"))
+    end
+
+    # Run monte carlo trials
+    set_model!(mcs, FUND)
+    run_mcs(mcs, trials; output_dir = "$output/results")
+
 end
-
-# Run monte carlo trials
-set_model!(mcs, FUND)
-run_mcs(mcs; 
-    post_trial_func = post, 
-    output_dir = "$output/results")
