dd3975734d7d99506c191cd0450c130e4b3aa6ac
Cora Kingdon <ckingdon@berkeley.edu>
2019-09-11 11:38:18 -0400

    add dual scc example

---
+++ examples/dual.jl
@@ -0,0 +1,5 @@
+using MimiFUND
+
+MimiFUND.compute_scco2_dual(year = 2020, prtp = 0.015)
+MimiFUND.compute_scco2_dual(year = 2020, prtp = 0.03)
+MimiFUND.compute_scco2_dual(year = 2020, prtp = 0.05)
