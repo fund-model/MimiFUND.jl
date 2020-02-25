686e445158eae4a5eb5ef9fdc638436636efe5d3
Kingdon <kingdon@rff.org>
2020-02-25 14:59:27 -0500

    switch signs of impact variables to be damages

--- src/components/ImpactHeatingComponent.jl
+++ src/components/ImpactHeatingComponent.jl
@@ -1,7 +1,7 @@
 ﻿@defcomp impactheating begin
     regions = Index()
 
-    heating = Variable(index=[time,regions])
+    heating = Variable(index=[time,regions])    # Economic damages: positive value means an increase in expenditures, negative value means a decrease
 
     hebm = Parameter(index=[regions])
     gdp90 = Parameter(index=[regions])
@@ -24,7 +24,7 @@
                 ypc = p.income[t, r] / p.population[t, r] * 1000.0
                 ypc90 = p.gdp90[r] / p.pop90[r] * 1000.0
 
-                v.heating[t, r] = p.hebm[r] * p.cumaeei[t, r] * p.gdp90[r] * atan(p.temp[t, r]) / atan(1.0) * (ypc / ypc90)^p.heel * p.population[t, r] / p.pop90[r]
+                v.heating[t, r] = -1 * p.hebm[r] * p.cumaeei[t, r] * p.gdp90[r] * atan(p.temp[t, r]) / atan(1.0) * (ypc / ypc90)^p.heel * p.population[t, r] / p.pop90[r]
             end
         end
     end
