fe397a92a4b2e27f3d621bd17ce6bbf72ebbc399
Cora Kingdon <ckingdon@berkeley.edu>
2020-07-17 11:57:59 -0400

    update to Mimi 1.0

--- src/components/ClimateCH4CycleComponent.jl
+++ src/components/ClimateCH4CycleComponent.jl
@@ -18,7 +18,7 @@
         if is_first(t)
             v.ch4decay = 1.0 / p.lifech4
 
-            v.acch4[1] = 1222.0
+            v.acch4[t] = 1222.0
         else
             # Calculate CH4 concentrations
             v.acch4[t] = v.acch4[t - 1] + 0.3597 * p.globch4[t] - v.ch4decay * (v.acch4[t - 1] - p.ch4pre)
